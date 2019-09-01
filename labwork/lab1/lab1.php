df5d90682a206d78b7f541c2c618ddf8d7bce1e5
EC2 Default User <ec2-user@ip-172-31-83-160.ec2.internal>
2019-09-01 19:44:54 +0000

    My first commit

---
+++ labwork/lab1/lab1.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+<title> Labwork 1: Hello World</title>
+<!--Lab1,
+Safa Abdalla,
+08/28/2019 --></head>
+<body>
+<h1>Safa Abdalla</h1>
+<p>Hey I am taking webdevelopment classes at LBCC,
+this is my first experince with PHP, 
+i feeel i willl enjoy this class.</p>
+<p>Favorite Hobbies</p>
+<ul>
+  <li>Swimming</li>
+  <li>Jogging</li>
+  <li>Yoga</li>
+</ul>
+<a href="https://github.com/safaaa01">This is my github profile page </a>
+
+<?php
+echo '<a href="https://github.com/safaaa01">This is my github profile;>'
+?>
+</body>
+</html>
